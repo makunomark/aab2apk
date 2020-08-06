13be5dc66c86df246bbd25396e3636058754d89e
Mark Makuno <makunomark@gmail.com>
2020-08-06 06:55:48 +0300

    Set up Vuex

---
+++ src/store/modules/file.js
@@ -0,0 +1,27 @@
+export const namespaced = true
+
+export const state = {
+  file: null,
+}
+
+export const mutations = {
+  ADD_FILE(state, file) {
+    state.file = file
+  },
+  REMOVE_FILE(state) {
+    state.file = null
+  },
+}
+
+export const actions = {
+  addFile({ commit }, file) {
+    commit('ADD_FILE', file)
+  },
+  removeFile({ commit }) {
+    commit('REMOVE_FILE')
+  },
+}
+
+export const modules = {}
+
+export const getters = {}
