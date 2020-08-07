164b7d4732405cb97511ebfce05bde6978dcdab8
Mark Makuno <makunomark@gmail.com>
2020-08-07 09:30:11 +0300

    Ui changes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app" class="bg-gray-100 h-screen flex items-center flex-col">
     <div class="my-4">
-      <img alt="Vue logo" src="./assets/logo.png" width="80" />
+      <img alt="Vue logo" src="./assets/logo.png" width="82" />
     </div>
 
     <template v-if="app.bundletoolExists">
